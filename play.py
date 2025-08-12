3f4e9ca86d9e43763f4f0ab2d18d387c5e849dd9
Quantum-TIM <145358437+Quantum-TIM@users.noreply.github.com>
2025-08-12 21:49:36 +0530

    Update play.py

--- play.py
+++ play.py
@@ -7,10 +7,11 @@ pyautogui.useImageNotFoundException()
 
 while True:
     try:
+        # locate the auto start condition image
         start = pyautogui.locateOnScreen("D:\Workspace\python\Projects\RDR2_starter\story.png", confidence=0.7)
         if start:
             time.sleep(4)
-            keyboard.press_and_release('enter')
+            keyboard.press_and_release('enter') # trigger key to start the game
             with open('game_start.txt', 'a+') as file:
                 cont = f"Game started at: {datetime.now()}\n"
                 file.write(cont)
@@ -19,5 +20,6 @@ while True:
             time.sleep(3)
             with open('game_start.png', 'a+') as file:
                 file.write(f"Game couldn't {datetime.now()}")
+    # sadly you will face this error often
     except pyautogui.ImageNotFoundException:
         pass
